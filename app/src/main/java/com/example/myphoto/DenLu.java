604b5726db5ce3927482ca45cfeec29f57091eb5
Guan612 <tiantony53@163.com>
2023-10-24 22:05:57 +0800

    登录注册控件

---
+++ app/src/main/java/com/example/myphoto/DenLu.java
@@ -0,0 +1,5 @@
+package com.example.myphoto;
+
+public class DenLu {
+
+}
---
+++ app/src/main/java/com/example/myphoto/ZuChe.java
@@ -0,0 +1,8 @@
+package com.example.myphoto;
+
+import android.content.Intent;
+import android.widget.Button;
+
+public class ZuChe {
+    
+}
