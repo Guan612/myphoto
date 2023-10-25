180a0ef71f92915b0b4022faf374c499df3ecd03
Guan612 <tiantony53@163.com>
2023-10-25 09:22:34 +0800

    满是bug的登录
    啊啊啊啊啊会疯的

--- app/src/main/java/com/example/myphoto/DenLu.java
+++ app/src/main/java/com/example/myphoto/DenLu.java
@@ -1,5 +1,23 @@
 package com.example.myphoto;
 
-public class DenLu {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 
+public class DenLu {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 其他初始化代码...
+    }
+
+    public void onButtonClick(View view) {
+        // 创建一个 Intent 对象，指定当前上下文和目标 Activity 类
+        Intent intent = new Intent(this, ZuChe.class);
+
+        // 启动目标 Activity
+        startActivity(intent);
+    }
 }
