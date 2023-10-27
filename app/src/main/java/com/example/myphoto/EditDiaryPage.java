352b1f9c347c42fb02fac5fd46a9f3ac3a85e0db
Guan612 <tiantony53@163.com>
2023-10-27 20:30:05 +0800

    预留原生写日记页面

---
+++ app/src/main/java/com/example/myphoto/EditDiaryPage.java
@@ -0,0 +1,16 @@
+package com.example.myphoto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditDiaryPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editdiarypage);
+
+
+    }
+}
