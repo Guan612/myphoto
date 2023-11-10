a5a32fae1cb04f46a5a7c4e907ab027d6630d6f6
Guan612 <tiantony53@163.com>
2023-11-10 16:56:27 +0800

    写了一大堆bug
    大模型救我狗命

--- app/src/main/java/com/example/myphoto/DenLuPage.java
+++ app/src/main/java/com/example/myphoto/DenLuPage.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class DenLuPage extends AppCompatActivity {
 
@@ -27,16 +28,52 @@ public class DenLuPage extends AppCompatActivity {
 
         //跳转主页
         Button loginButton=findViewById(R.id.login);
+
         loginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent();
-                intent.setClassName("com.example.myphoto","com.example.myphoto.MainActivity");
-                startActivity(intent);
+                EditText usernameEditText = findViewById(R.id.userid);
+                EditText passwordEditText = findViewById(R.id.password);
+
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                // 执行登录验证逻辑
+                if (verifyCredentials(username, password)) {
+                    // 验证成功，进行页面跳转
+                    // 在这里添加跳转逻辑，例如启动下一个Activity或跳转到主界面
+                    Intent intent=new Intent();
+                    intent.setClassName("com.example.myphoto","com.example.myphoto.MainActivity");
+                    startActivity(intent);
+                } else {
+                    // 验证失败，给出相应的提示或处理逻辑
+                }
+
             }
         });
 
         //登录发送信息给后台
+
+    }
+
+    //数据库相关部分
+    private boolean verifyCredentials(String username, String password) {
+        MyDatabaseHelper databaseHelper = new MyDatabaseHelper(getApplicationContext());
+        SQLiteDatabase db = databaseHelper.getReadableDatabase();
+
+        // 执行查询操作
+        String[] projection = {"username"};
+        String selection = "username = ? AND password = ?";
+        String[] selectionArgs = {username, password};
+        Cursor cursor = db.query("users", projection, selection, selectionArgs, null, null, null);
+
+        boolean credentialsMatch = cursor.moveToFirst();
+
+        // 关闭数据库连接和游标
+        cursor.close();
+        db.close();
+
+        return credentialsMatch;
 
     }
 
--- app/src/main/java/com/example/myphoto/ZuChePage.java
+++ app/src/main/java/com/example/myphoto/ZuChePage.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import java.io.IOException;
 import java.net.URL;
@@ -14,5 +19,31 @@ public class ZuChePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.zuchepage);
+
+        EditText userid = findViewById(R.id.userid);
+        EditText password = findViewById(R.id.password);
+        RadioButton allowUserProtocol = findViewById(R.id.allowUserProtocol);
+        Button register = findViewById(R.id.register);
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText userIdEditText = findViewById(R.id.userid);
+                EditText passwordEditText = findViewById(R.id.password);
+                RadioButton allowUserProtocolRadioButton = findViewById(R.id.allowUserProtocol);
+
+                String userId = userIdEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                boolean allowUserProtocol = allowUserProtocolRadioButton.isChecked();
+
+                if (allowUserProtocol) {
+                    // 在这里执行注册逻辑和数据库写入操作
+                    // 可以调用相关的数据库操作方法，将用户名和密码写入数据库
+                } else {
+                    // 用户未同意用户协议，给出相应的提示或处理逻辑
+                }
+            }
+        });
     };
 }
