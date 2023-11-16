81768a93c48c883e7e3a324340b87d40fb2468e7
Guan612 <tiantony53@163.com>
2023-11-16 16:26:06 +0800

    修改了编辑日记

--- app/src/main/java/com/example/myphoto/EditDiaryPage.java
+++ app/src/main/java/com/example/myphoto/EditDiaryPage.java
@@ -2,7 +2,12 @@ package com.example.myphoto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditDiaryPage extends AppCompatActivity {
 
@@ -11,6 +16,21 @@ public class EditDiaryPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editdiarypage);
 
+        //获取日记数据
+        EditText editTitle = findViewById(R.id.editTitle);
+        EditText editContent = findViewById(R.id.editContent);
+        Button edSave = findViewById(R.id.edSave);
+
+        //按下按钮时保存日记
+        edSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String diaryTitle = editTitle.getText().toString().trim();
+                String diaryContent = editContent.getText().toString().trim();
+            }
+        });
 
     }
+
+
 }
