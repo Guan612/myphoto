3587def93a4241c4fe606617102d98ede4139ed3
Guan612 <tiantony53@163.com>
2023-11-16 16:26:24 +0800

    Update MePage.java

--- app/src/main/java/com/example/myphoto/MePage.java
+++ app/src/main/java/com/example/myphoto/MePage.java
@@ -16,7 +16,7 @@ public class MePage extends AppCompatActivity {
 
         Button goMainactivy = findViewById(R.id.goMainactivy);
         Button goLogin = findViewById(R.id.goLogin);
-
+        Button goAbout = findViewById(R.id.AboutMe);
         //返回主页跳转
         goMainactivy.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -33,8 +33,19 @@ public class MePage extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent();
                 intent.setClassName("com.example.myphoto","com.example.myphoto.DenLuPage");
+                startActivity(intent);
             }
         });
+        //关于页面跳转
+        goAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClassName("com.example.myphoto","com.example.myphoto.AboutPage");
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
