ac853e54674bbb8b273ea22c55dec46eba3c6bd9
Guan612 <tiantony53@163.com>
2024-01-12 15:41:38 +0800

    Update MyDatabase.java

--- app/src/main/java/com/example/myphoto/MyDatabase.java
+++ app/src/main/java/com/example/myphoto/MyDatabase.java
@@ -8,13 +8,27 @@ import androidx.annotation.Nullable;
 
 public class MyDatabase extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME = "mydatabase.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "mytable";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_diaryTitle = "diaryTitle";
+    private static final String COLUMN_diaryContent = "diaryContent";
+    private static final String COLUMN_timestamp = "timestamp";
+    private Object createTableQuery;
+
     public MyDatabase(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String createTableQuery = "CREATE TABLE " + TABLE_NAME + "(" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_diaryTitle + " TEXT, " +
+                COLUMN_diaryContent + " TEXT, " +
+                COLUMN_timestamp + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+        sqLiteDatabase.execSQL(createTableQuery);
     }
 
     @Override
